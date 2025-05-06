754186df325106c9e925fc0ce3d0a6fced1142a5
Xin chao mn <mactrung116@gmail.com>
2025-05-06 15:24:20 +0700

    update code 6/5/2025

---
+++ ThucHanh/BeeLibrary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel.Syndication;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ThucHanh
+{
+    class BeeLibrary
+    {
+        public static async Task<List<RssItem>> GetRssFeedAsync(string url)
+        {
+            return await Task.Run(() =>
+            {
+                var rssItems = new List<RssItem>();
+
+                using (var reader = XmlReader.Create(url))
+                {
+                    var feed = SyndicationFeed.Load(reader);
+                    foreach (var item in feed.Items)
+                    {
+                        rssItems.Add(new RssItem
+                        {
+                            Title = item.Title.Text,
+                            Description = item.Summary?.Text ?? "",
+                            PubDate = item.PublishDate.ToString("yyyy-MM-dd HH:mm")
+                        });
+                    }
+                }
+
+                return rssItems;
+            });
+        }
+    }
+}
--- ThucHanh/Form1.cs
+++ ThucHanh/Form1.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Web.WebView2.WinForms;
@@ -13,19 +10,53 @@ namespace ThucHanh
 {
     public partial class Form1 : Form
     {
+        private WebView2 webView;
         public Form1()
         {
             InitializeComponent();
-            var webView = new WebView2
+            webView = new WebView2
             {
                 Dock = DockStyle.Fill
             };
             this.Controls.Add(webView);
-            webView.Source = new Uri("https://www.fc-soft.jp/campaign/y_naming2025/");
+            webView.CoreWebView2InitializationCompleted += WebView_CoreWebView2InitializationCompleted;
+            webView.EnsureCoreWebView2Async();
         }
-		async void LoadListViewData()
-		{
-			string rssUrl = "https://www.fc-soft.jp/dcms-rss/news_k/";
-		}
-	}
+
+        private async void WebView_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            await LoadAndDisplayRssAsync();
+        }
+        private async Task LoadAndDisplayRssAsync()
+        {
+            string rssUrl = "https://www.fc-soft.jp/dcms-rss/news_k/";
+            try
+            {
+                List<RssItem> rssItems = await BeeLibrary.GetRssFeedAsync(rssUrl);
+                Cursor.Current = Cursors.WaitCursor;
+                if (rssItems == null || rssItems.Count == 0)
+                {
+                    webView.NavigateToString("データ取得に失敗しました。");
+                    return;
+                }
+
+                var sortedList = rssItems.OrderByDescending(a => a.PubDate).Take(10).ToList();
+
+                string html = "<html><head><meta charset='UTF-8'></head><body style='font-family: Meiryo UI; font-size: 16px;'><table style='width: 100%'>";
+                foreach (var item in sortedList)
+                {
+                    string desc = item.Description.Replace("\n", "<br>");
+                    html += $"<tr valign='top'><td style='width:350px;'>{item.PubDate}</td><td style='width: 50px;'>&nbsp;</td><td><h3>{item.Title}</h3>{desc}<hr/></td></tr>";
+                }
+                html += "</table></body></html>";
+
+                webView.NavigateToString(html);
+            }
+            catch (Exception ex)
+            {
+                webView.NavigateToString($"<p>Lỗi khi tải RSS: {ex.Message}</p>");
+            }    
+        }
+    }
 }
+
