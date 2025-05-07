269b0c8df7827f66096802f8ecbd93f6b25d554d
Xin chao mn <mactrung116@gmail.com>
2025-05-07 16:57:53 +0700

    update 7/5 16:59

--- ThucHanh/Form1.Designer.cs
+++ ThucHanh/Form1.Designer.cs
@@ -29,20 +29,38 @@ namespace ThucHanh
         /// </summary>
         private void InitializeComponent()
         {
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             this.SuspendLayout();
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1543, 813);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1233, 637);
+            this.ClientSize = new System.Drawing.Size(1234, 650);
+            this.Controls.Add(this.webView21);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
     }
 }
 
--- ThucHanh/Form1.cs
+++ ThucHanh/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -10,44 +11,90 @@ using Microsoft.Web.WebView2.WinForms;
 namespace ThucHanh
 {
     public partial class Form1 : Form
-    {
-        private WebView2 webView;
+    {       
         public Form1()
         {
             InitializeComponent();
-            webView = new WebView2
+
+            
+            //webView21.CoreWebView2InitializationCompleted += async (s, e) =>
+            //{
+            //    if (e.IsSuccess)
+            //    {
+            //        webView21.NavigationStarting += CoreWebView2_NavigationStarting;
+            //        await LoadAndDisplayRssAsync();
+            //    }
+
+            //    await LoadAndDisplayRssAsync();
+            //};
+
+            InitializeWebView2Async();
+
+            //task.r await LoadAndDisplayRssAsync();
+            webView21.EnsureCoreWebView2Async(); //đảm bảo WebView2 được khởi tạo đúng cách
+        }
+
+        private async void InitializeWebView2Async()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+
+            webView21.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting2;
+
+            // Gọi hàm lấy dữ liệu RSS và hiển thị
+            await LoadAndDisplayRssAsync();
+        }
+
+        // Xử lý sự kiện khi click vào liên kết
+        private void CoreWebView2_NavigationStarting2(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            // Kiểm tra nếu URL là liên kết bên ngoài (bắt đầu bằng http hoặc https)
+            if (e.Uri.StartsWith("http://") || e.Uri.StartsWith("https://") || e.Uri.EndsWith(".pdf"))
             {
-                Dock = DockStyle.Fill 
-            };
-            this.Controls.Add(webView);
-            webView.CoreWebView2InitializationCompleted += async (s, e) =>
-            {
-                if (e.IsSuccess)
+                // Hủy điều hướng trong WebView2
+                e.Cancel = true;
+
+                // Mở liên kết trong trình duyệt mặc định
+                try
                 {
-                    webView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
-                    await LoadAndDisplayRssAsync();
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = e.Uri,
+                        UseShellExecute = true
+                    });
                 }
-            };
-            webView.EnsureCoreWebView2Async(); //đảm bảo WebView2 được khởi tạo đúng cách
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Không thể mở liên kết: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
 
         private void CoreWebView2_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
         {
-            e.Cancel = true;
-            string chromePath = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
             string url = e.Uri;
-            try
+
+            if (url.StartsWith("http://") || url.StartsWith("https://"))
             {
-                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                e.Cancel = true;
+
+                try
                 {
-                    FileName = chromePath,
-                    Arguments = url,
-                    UseShellExecute = false
-                });
+                    string chromePath = @"C:\Program Files\Google\Chrome\Application\chrome.exe"; //link dẫn đến app Chrome 
+                    System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                    {
+                        FileName = chromePath,
+                        Arguments = url,
+                        UseShellExecute = true
+                    });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Không thể mở Chrome: " + ex.Message);
+                }
             }
-            catch (Exception ex)
+            else
             {
-                MessageBox.Show("Không thể mở Chrome: " + ex.Message);
+                e.Cancel = false;
             }
         }
         private async Task LoadAndDisplayRssAsync()
@@ -59,25 +106,24 @@ namespace ThucHanh
                 Cursor.Current = Cursors.WaitCursor; 
                 if (rssItems == null || rssItems.Count == 0)
                 {
-                    webView.NavigateToString("データ取得に失敗しました。"); //báo không lấy được dữ liệu nếu không có dữ liệu
+                    webView21.NavigateToString("データ取得に失敗しました。"); //báo không lấy được dữ liệu nếu không có dữ liệu
                     return;
                 }
 
                 //tạo HTML để hiển thị
                 var sortedList = rssItems.OrderByDescending(a => a.PubDate).Take(10).ToList(); //sắp xếp theo ngày mới nhất
-                string text = "<table style='width: 100%'>";
+                string text = "<table style='width: 100%'>"; 
                 foreach (RssItem serviceInfo in sortedList)
                 {
                     string desc = serviceInfo.Description.Replace("\n", "<br>");
-                    text += $"<tr valign='top'><td style='width:350px;'>{serviceInfo.PubDate}</td><td style='width: 50px;'>&nbsp;</td><td><h3>{serviceInfo.Title}</h3>{desc}<hr/></td></tr>"; 
+                    text += $"<tr valign='top'><td style='width:350px;'>{serviceInfo.PubDate}</td><td style='width: 20px;'>&nbsp;</td><td><h3>{serviceInfo.Title}</h3>{desc}<hr/></td></tr>"; 
                 }
                 text += "</table></body></html>";
-
-                webView.NavigateToString(text);
+                webView21.NavigateToString(text);
             }
             catch (Exception ex)
             {
-                webView.NavigateToString($"Lỗi khi tải RSS: {ex.Message}");
+                webView21.NavigateToString($"Lỗi khi tải RSS: {ex.Message}");
             }    
         }
     }
