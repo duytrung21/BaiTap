ea172a9343a1c0b035a6beb0195052b6af55352f
Xin chao mn <mactrung116@gmail.com>
2025-05-08 11:52:54 +0700

    11:55

--- ThucHanh/Form1.cs
+++ ThucHanh/Form1.cs
@@ -24,15 +24,18 @@ namespace ThucHanh
 
             webView21.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting2;
 
+            // Đăng ký sự kiện NewWindowRequested cho các liên kết có target='_blank'
+            webView21.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+
             // Gọi hàm lấy dữ liệu RSS và hiển thị
             await LoadAndDisplayRssAsync();
         }
 
-        // Xử lý sự kiện khi click vào liên kết
+        //// Xử lý sự kiện khi click vào liên kết
         private void CoreWebView2_NavigationStarting2(object sender, CoreWebView2NavigationStartingEventArgs e)
         {
             // Kiểm tra nếu URL là liên kết bên ngoài (bắt đầu bằng http hoặc https)
-            if (e.Uri.StartsWith("http://") || e.Uri.StartsWith("https://") || e.Uri.EndsWith(".pdf"))
+            if (e.Uri.StartsWith("http://") || e.Uri.StartsWith("https://"))
             {
                 // Hủy điều hướng trong WebView2
                 e.Cancel = true;
@@ -53,6 +56,27 @@ namespace ThucHanh
             }
         }
 
+        // Xử lý sự kiện khi click vào liên kết có target='_blank'
+        private void CoreWebView2_NewWindowRequested(object sender, CoreWebView2NewWindowRequestedEventArgs e)
+        {
+            // Hủy hành động mở cửa sổ mới của WebView2
+            e.Handled = true;
+
+            // Mở liên kết trong trình duyệt mặc định
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = e.Uri,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Không thể mở liên kết: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
         private async Task LoadAndDisplayRssAsync()
         {
             string rssUrl = "https://www.fc-soft.jp/dcms-rss/news_k/";
@@ -69,16 +93,15 @@ namespace ThucHanh
                 //tạo HTML để hiển thị
                 var sortedList = rssItems.OrderByDescending(a => a.PubDate).Take(10).ToList(); //sắp xếp theo ngày mới nhất
                 string text1 = "<table style='width: 100%'>"; 
-                string text2 = "<table style='width: 100%'>"; 
+                //string text2 = "<table style='width: 100%'>"; 
                 foreach (RssItem serviceInfo in sortedList)
                 {
                     string desc = serviceInfo.Description.Replace("\n", "<br>");
                     text1 += $"<tr valign='top'><td style='width:350px;'>{serviceInfo.PubDate}</td><td style='width: 20px;'>&nbsp;</td><td><h3>{serviceInfo.Title}</h3>{desc}<hr/></td></tr>"; 
                 }
                 text1 += "</table></body></html>";
-                //text1 += "<a href=\"https://s3-ap-northeast-1.amazonaws.com/fcdownload/PDF/R0704_menu.pdf\" target=\"_blank\" rel=\"noopener\">詳しくはこちら</a>"; //test link
-                text2 += text1.Replace(" target=\"_blank\" rel=\"noopener\"", "");//bỏ target = blank để hiển thị thông tin trên Chorme
-                webView21.NavigateToString(text2);
+                //text2 += text1.Replace(" target=\"_blank\" rel=\"noopener\"", "");//bỏ target = blank để hiển thị thông tin trên Chorme
+                webView21.NavigateToString(text1);
             }
             catch (Exception ex)
             {
